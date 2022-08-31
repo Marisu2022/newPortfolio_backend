171abe77d3c8065a35cb981a98986ebc52117180
María Susana Ferro (Marisú) <94333828+Marisu2022@users.noreply.github.com>
2022-08-30 22:11:36 -0300

    Update HeaderController.java

--- src/main/java/com/portafolio/api/Controller/HeaderController.java
+++ src/main/java/com/portafolio/api/Controller/HeaderController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping()
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://marisu-2679a.web.app")
 public class HeaderController {
 
     @Autowired
