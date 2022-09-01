3014fd1ab16e8d4960bce28d96719feb57cbcc6c
María Susana Ferro (Marisú) <94333828+Marisu2022@users.noreply.github.com>
2022-09-01 10:19:14 -0300

    Update SkillController.java

--- src/main/java/com/portafolio/api/Controller/SkillController.java
+++ src/main/java/com/portafolio/api/Controller/SkillController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping()
-@CrossOrigin(origins = " * ")
+@CrossOrigin
 public class SkillController {
   @Autowired
 private ISkillService skServ;  
