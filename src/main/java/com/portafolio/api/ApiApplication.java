94c9de74e9bc52e547680e543ccc9d2bb6f904e9
María Susana Ferro (Marisú) <94333828+Marisu2022@users.noreply.github.com>
2022-09-01 17:44:51 -0300

    Update ApiApplication.java

--- src/main/java/com/portafolio/api/ApiApplication.java
+++ src/main/java/com/portafolio/api/ApiApplication.java
@@ -18,7 +18,7 @@ public class ApiApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://marisu-2679a.web.app/"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://marisu-2679a.web.app"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
