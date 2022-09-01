b2a583bfc15024bdc460efaf4ca91aa398560dd2
María Susana Ferro (Marisú) <94333828+Marisu2022@users.noreply.github.com>
2022-09-01 10:26:41 -0300

    Update MainSecurity.java

--- src/main/java/com/portafolio/api/Security/MainSecurity.java
+++ src/main/java/com/portafolio/api/Security/MainSecurity.java
@@ -6,6 +6,7 @@ import com.portafolio.api.Security.jwt.JwtEntryPoint;
 import com.portafolio.api.Security.jwt.JwtTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
