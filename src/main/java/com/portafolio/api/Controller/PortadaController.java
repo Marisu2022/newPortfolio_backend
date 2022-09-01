369725435ccb35b7b392021ac94a9d09d00c5227
María Susana Ferro (Marisú) <94333828+Marisu2022@users.noreply.github.com>
2022-09-01 10:18:22 -0300

    Update PortadaController.java

--- src/main/java/com/portafolio/api/Controller/PortadaController.java
+++ src/main/java/com/portafolio/api/Controller/PortadaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping()
-@CrossOrigin(origins = "*")
+@CrossOrigin
 public class PortadaController {
 
     @Autowired
