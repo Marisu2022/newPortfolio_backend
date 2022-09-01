f36a907807c2456b1a4ce61e2a0c3c46fcceedcd
María Susana Ferro (Marisú) <94333828+Marisu2022@users.noreply.github.com>
2022-09-01 00:26:32 -0300

    Update AuthController.java

--- src/main/java/com/portafolio/api/Security/Controller/AuthController.java
+++ src/main/java/com/portafolio/api/Security/Controller/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://marisu-2679a.web.app")
+@CrossOrigin(origins = " * ")
 public class AuthController {
 
     @Autowired
