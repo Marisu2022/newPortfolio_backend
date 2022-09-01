ced7627b449b9c82549dac6231074abb956ca2c2
María Susana Ferro (Marisú) <94333828+Marisu2022@users.noreply.github.com>
2022-09-01 10:16:25 -0300

    Update EducationController.java

--- src/main/java/com/portafolio/api/Controller/EducationController.java
+++ src/main/java/com/portafolio/api/Controller/EducationController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping()
-@CrossOrigin(origins = " * ")
+@CrossOrigin
 public class EducationController {
 
     @Autowired
