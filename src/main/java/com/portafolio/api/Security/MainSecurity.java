c81daa31824c952b13c0f254c623fae9549e1e15
María Susana Ferro (Marisú) <94333828+Marisu2022@users.noreply.github.com>
2022-09-01 11:21:35 -0300

    Update MainSecurity.java

--- src/main/java/com/portafolio/api/Security/MainSecurity.java
+++ src/main/java/com/portafolio/api/Security/MainSecurity.java
@@ -6,7 +6,7 @@ import com.portafolio.api.Security.jwt.JwtEntryPoint;
 import com.portafolio.api.Security.jwt.JwtTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -62,7 +62,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
                  .antMatchers("/auth/**").permitAll()
-                .antMatchers(HttpMethod.GET).permitAll()
+           
            .anyRequest().authenticated()                
                 .and()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
